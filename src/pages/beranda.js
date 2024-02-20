8f9d0cd4260ff532efece4902478a71bf2b009bf
Maharaniiibp <maharanibanicaputri@gmail.com>
2024-02-20 12:09:42 +0700

    update beranda

--- src/pages/beranda.js
+++ src/pages/beranda.js
@@ -4,93 +4,156 @@ import { Link } from "react-router-dom";
 const Beranda = () => {
   return (
     <>
-    <div className="py-5">
-    <section className="satu">
-        <div className="d-flex mx-5">
-          <div
-            style={{
-              color: "black",
-              fontSize: 36,
-              fontFamily: "Inter, sans-serif",
-              fontWeight: "600",
-            }}
-          >
-            Beranda
-          </div>
-          <div className="ms-auto">
-            <img
-              style={{
-                width: 56,
-                height: 56,
-                borderRadius: 50,
-                border: "3px #009B4C solid",
-              }}
-              src="../assets/tanglet.svg"
-              alt="Tanglet Logo"
-            />
-          </div>
-        </div>
-      </section>
-
-      <section className="dua">
-        <div className="p-sm-5 d-sm-flex align-items-center">
-          <div className="flex-shrink-0">
+      <div className="py-5">
+        <section className="satu">
+          <div className="d-flex mx-5">
             <div
-              className="p-2 rounded-md"
-              style={{ backgroundColor: "#198754", borderRadius: "10px" }}
+              style={{
+                color: "black",
+                fontSize: 36,
+                fontFamily: "Inter, sans-serif",
+                fontWeight: "600",
+              }}
             >
-              <div className="mb-1 mx-2 p-2">
-                <img
-                  src="./assets/pcnu.png"
-                  alt="gambar pcnu"
-                  className="img-fluid"
-                />
+              Beranda
+            </div>
+            <div className="ms-auto">
+              <img
+                style={{
+                  width: 56,
+                  height: 56,
+                  borderRadius: 50,
+                  border: "3px #009B4C solid",
+                }}
+                src="../assets/tanglet.svg"
+                alt="Tanglet Logo"
+              />
+            </div>
+          </div>
+        </section>
+
+        <section className="dua py-4">
+          <div class="row row-cols-1 row-cols-md-2 g-4 mx-4">
+            <div class="col ">
+              <div class="card h-90 bg-success">
+                <div class="card-body">
+                  <Link to="/beranda">
+                    <h5 class="card-title fw-bold mx-3 text-white ">Beranda</h5>
+                  </Link>
+                  <div class="card-body bg-white rounded">
+                    <p class="card-text text-black mb-0">data per page</p>
+                  </div>
+                </div>
+                <div className="py-4"></div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card h-90 bg-success">
+                <div class="card-body">
+                  <Link to="/koin">
+                    <h5 class="card-title fw-bold mx-3 text-white  ">
+                      Campaign Program
+                    </h5>
+                  </Link>
+                  <div class="card-body bg-white rounded">
+                    <p class="card-text text-success mb-0">1 disetujui</p>
+                    <p class="card-text text-warning mb-0">1 pengajuan</p>
+                    <p class="card-text text-danger mb-0">1 ditolak</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card h-90 bg-success">
+                <div class="card-body">
+                  <Link to="/donatur">
+                    <h5 class="card-title fw-bold mx-3 text-white ">Donatur</h5>
+                  </Link>
+                  <div class="card-body bg-white rounded">
+                    <p class="card-text text-success mb-0">3 transaksi</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-90 bg-success">
+                <div class="card-body">
+                  <Link to="/nderektanglet">
+                    <h5 class="card-title fw-bold mx-3 text-white ">
+                      Nderek Tanglet
+                    </h5>
+                  </Link>
+                  <div class="card-body bg-white rounded">
+                    <p class="card-text text-black mb-0">4 chat</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card h-90 bg-success">
+                <div class="card-body">
+                  <Link to="/konten">
+                    <h5 class="card-title fw-bold mx-3 text-white ">Berita</h5>
+                  </Link>
+                  <div class="card-body bg-white rounded">
+                    <p class="card-text text-warning mb-0">1 draft</p>
+                    <p class="card-text text-success mb-0">1 publish</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card h-90 bg-success">
+                <div class="card-body">
+                  <Link to="/artikel">
+                    <h5 class="card-title fw-bold mx-3 text-white ">Artikel</h5>
+                  </Link>
+                  <div class="card-body bg-white rounded">
+                    <p class="card-text text-warning mb-0">1 draft</p>
+                    <p class="card-text text-success mb-0">1 publish</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card h-90 bg-success">
+                <div class="card-body">
+                  <Link to="/events">
+                    <h5 class="card-title fw-bold mx-3 text-white ">Event</h5>
+                  </Link>
+                  <div class="card-body bg-white rounded">
+                    <p class="card-text text-danger mb-0">Youtube video</p>
+                    <p class="card-text text-danger mb-0">Instagram post</p>
+                    <p class="card-text text-danger mb-0">Instagram post</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card h-90 bg-success">
+                <div class="card-body">
+                  <Link to="/pengaturan">
+                    <h5 class="card-title fw-bold mx-3 text-white ">
+                      Pengaturan
+                    </h5>
+                  </Link>
+                  <div class="card-body bg-white rounded">
+                    <p class="card-text text-black mb-0">detail akun</p>
+                    <p class="card-text text-danger mb-0">security</p>
+                  </div>
+                </div>
+                <div className="py-2"></div>
               </div>
             </div>
           </div>
-          <div
-            className="flex-grow-1 ms-5 fw-bold fs-3"
-            style={{ fontFamily: "Inter, sans-serif" }}
-          >
-            PCNU Kota Batu, mendigdayakan <br /> NU menjemput abad kedua menuju{" "}
-            <br /> kebangkitan baru.
-            <div className="fs-6 mt-4 fw-medium">
-              Bertempat di Jl. Agus Salim No.21 Sisir, kec. Batu, Kota Batu,
-              PCNU <br />
-              Batu memiliki tugas utama yaitu mengatur dan mengelola roda
-              organisasi <br />
-              di tingkat cabang, agar roda organisasi dapat berjalan dengan
-              terarah <br />
-              dan dinamis sesuai dengan keberadaan dan kebutuhan NU yang ada di
-              kota <br />
-              batu secara khusus. Sama halnya dengan PCNU cabang lainnya, PCNU
-              Kota <br />
-              batu juga memiliki struktur pengurus, badan-badan otonom, serta{" "}
-              <br />
-              program kerja.
-            </div>
-            <div className="mb-4"></div>
-            <Link to="/koin" style={{ textDecoration: "none" }}>
-              <button style={{ backgroundColor: "#198754" }} className="d-flex border border-0 rounded-2 p-2">
-                <div className="flex-shrink-0">
-                  <img src="./assets/coin.svg" alt="..." />
-                </div>
-                <div className="flex-grow-1 ms-2 mt-2 fs-6 fw-normal text-white">
-                  Koin NUsantara
-                </div>
-              </button>
-            </Link>
-          </div>
-        </div>
-      </section>
-
-      <section className="dua">
-        <div className="p-4" style={{ backgroundColor: "#EDF6F0" }}>
-          <p className="text-success fs-4 fw-bold">Nderek Tanglet</p>
-        </div>
-      </section>
-    </div>
-      
+        </section>
+      </div>
     </>
   );
 };
